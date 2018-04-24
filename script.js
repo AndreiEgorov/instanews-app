d89e3214a3beb7c7949e2c94c4d05f058c7256f2
Andrei Egorov <andreyegorov1992@gmail.com>
2018-04-24 10:50:59 -0700

    initial Commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// Built by LucyBot. www.lucybot.com
+var url = "https://api.nytimes.com/svc/topstories/v2/opinion.json";
+url += '?' + $.param({
+  'api-key': "64cca612ef9b4f8cb1bd909039043090"
+});
+$.ajax({
+  url: url,
+  method: 'GET',
+}).done(function(result) {
+  console.log(result);
+}).fail(function(err) {
+  throw err;
+});
