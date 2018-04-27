c6a388ad95729cfc610cb431c944a4472359f56d
Andrei Egorov <andreyegorov1992@gmail.com>
2018-04-27 13:48:50 -0700

    Finish mobile version

--- js/main.js
+++ js/main.js
@@ -1,3 +1,52 @@
+$(document).ready(function () {
+
+    // Built by LucyBot. www.lucybot.com
+
+    $("#section").on("change", function (data) {
+        $(".contents").css({ "transform": "scale(0.7)", "transition": "all 0.5s linear", "height": "350px", });
+
+
+        $("#loader").show();
+        $("#news li").remove();
+
+
+        var selected = $("#section").val();
+
+        var url = "https://api.nytimes.com/svc/topstories/v2/" + selected + ".json";
+        url += '?' + $.param({
+            'api-key': "64cca612ef9b4f8cb1bd909039043090"
+        });
+
+        $.ajax({
+            url: url,
+            method: 'GET',
+            dataType: 'json'
+        })
+            .done(function (data) {
+
+                var dataResults = data.results;
+
+                // new array based on dataResults, filter out items in the array that don't have any images and trim the results to 12
+                var filteredResults = dataResults.filter(function (index) {
+                    return index.multimedia.length;
+                }).slice(0, 12);
+
+                $.each(filteredResults, function (index, value) {
+
+
+                    var filteredImage = value.multimedia[4].url;
+                    var filteredAbstract = value.abstract;
+                    var filterdLink = value.url;
+
+
+
+
+                    $("#news").append("<li> <a href=" + [filterdLink] + "> <div class='newsbox' id='newssquare" + index + "' style='background-image: url(" + filteredImage + ")'></div> </a> </li>");
+                    $("#newssquare" + index).append("<p class='abstract'> " + [filteredAbstract] + " </p>");
+
+                    $("#loader").hide();
+
+                });
 
 
 
@@ -5,16 +54,37 @@
 
 
 
-// // Built by LucyBot. www.lucybot.com
-// var url = 'https://api.nytimes.com/svc/topstories/v2/opinion.json';
-// url += '?' + $.param({
-//   'api-key': '64cca612ef9b4f8cb1bd909039043090'
-// });
-// $.ajax({
-//   url: url,
-//   method: 'GET',
-// }).done(function(result) {
-//   console.log(result);
-// }).fail(function(err) {
-//   throw err;
-// });
+            });
+
+
+    })
+    // .fail(function (err) {
+    //     throw err;
+    // });
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // loop through the filtere array
+// for (var index = 0; index < filteredResults.length; index++) {
+
+//     // console.log(filteredImage);
+//     // var articleImage = dataResults[index].multimedia[4].url;
+//     // console.log(articleImage);
+//     console.log(filteredResults[index].multimedia[4].url);
+// }
+
+
